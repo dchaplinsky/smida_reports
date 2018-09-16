0032c3944293724a7f1c9e9f6d5eab0e5e4f1e4d
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2018-09-16 16:45:28 +0300

    Converting and saving data to the feed

--- downloader.py
+++ downloader.py
@@ -1,11 +1,29 @@
 import os
 import os.path
 import math
+import json
+import xmltodict
 from tqdm import tqdm
 from xml.etree import ElementTree
 
 import requests
 
+
+def convert_params(doc):
+    if isinstance(doc, dict):
+        for d in doc:
+            if d == "param" and isinstance(doc[d], (list, tuple)):
+                doc[d] = {
+                    node["@name"]: node.get("#text") for node in doc[d]
+                }
+            else:
+                doc[d] = convert_params(doc[d])
+    elif isinstance(doc, (list, tuple)):
+        doc = [
+            convert_params(d) for d in doc
+        ]
+
+    return doc
 
 def download_page(href, id_):
     fdir = os.path.join("cache", str(id_)[-1])
@@ -14,7 +32,10 @@ def download_page(href, id_):
 
     fname = os.path.join(fdir, "{}.xml".format(id_))
     if os.path.exists(fname):
-        return
+        with open(fname, "r") as fp:
+            s = fp.read()
+            if s:
+                return xmltodict.parse(s)
 
     r = requests.get("https://stockmarket.gov.ua/api/v1/{}".format(href))
 
@@ -25,6 +46,8 @@ def download_page(href, id_):
     with open(fname, "w") as fp:
         fp.write(r.text)
 
+    return xmltodict.parse(r.text)
+
 
 def download_index():
     r = requests.get("https://stockmarket.gov.ua/api/v1/issuer-report-index.xml", params={"limit": 0})
@@ -33,21 +56,24 @@ def download_index():
     id_min = int(index.attrib["id_min"])
     curr_id = int(index.attrib["id_max"])
 
-    with tqdm(total=num_pages) as pbar:
-        while curr_id > id_min:
-            pg = requests.get("https://stockmarket.gov.ua/api/v1/issuer-report-index.xml", params={"idlast": curr_id})
-            page = ElementTree.fromstring(pg.content)
+    with open("out.jsonlines", "w") as fp:
+        with tqdm(total=num_pages) as pbar:
+            while curr_id > id_min:
+                pg = requests.get("https://stockmarket.gov.ua/api/v1/issuer-report-index.xml", params={"idlast": curr_id})
+                page = ElementTree.fromstring(pg.content)
 
-            for item in page.findall(".//{http://stockmarket.gov.ua/api/v1/report-index.xsd}item"):
-                href = item.attrib["href"]
-                id_ = int(item.attrib["id"])
+                for item in page.findall(".//{http://stockmarket.gov.ua/api/v1/report-index.xsd}item"):
+                    href = item.attrib["href"]
+                    id_ = int(item.attrib["id"])
 
-                download_page(href, id_)
+                    res = convert_params(download_page(href, id_))
+                    if res is not None:
+                        fp.write(json.dumps(res, ensure_ascii=False) + "\n")
 
-                pbar.update(1)
+                    pbar.update(1)
 
-                if id_ < curr_id:
-                    curr_id = id_
+                    if id_ < curr_id:
+                        curr_id = id_
 
 
 if __name__ == '__main__':
